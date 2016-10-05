5598aa9e3d23fde662da61408e8460c9b098db52
Doug Wright <dwright@takepart.com>
2016-10-05 12:10:54 -0700

    Testing hook to Sidekiq ExceptionHandler Logger

--- lib/sidekiq/notifier.rb
+++ lib/sidekiq/notifier.rb
@@ -1,7 +1,19 @@
-require "sidekiq/notifier/version"
+require "uri"
+require "sidekiq"
+require "net/http"
 
 module Sidekiq
-  module Notifier
-    # WIP
+  module ExceptionHandler
+    class Notifier < Logger
+      def handle_exception(exception, context_hash={})
+        print "Testing this hook...."
+        super(exception, context_hash)
+      end
+    end
   end
 end
+
+
+# NOTES:
+# https://github.com/mperham/sidekiq/wiki/Error-Handling
+# http://guides.rubygems.org/make-your-own-gem/#introduction
